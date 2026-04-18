1248f26eb02d76bc4ec24205933c1656fffd9961
Kris <krishuang007@gmail.com>
2026-04-18 17:29:50 +0800

    加入 bbr 开启脚本

---
+++ bbr_start.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# start BBR
+if  [ ! -f "/etc/sysctl.conf.cp" ];then
+  sudo cp /etc/sysctl.conf /etc/sysctl.conf.cp
+  fi
+
+sudo echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
+sudo echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
+sudo sysctl -p
--- v2ray_install.sh
+++ v2ray_install.sh
@@ -1,14 +1,5 @@
 #!/bin/sh
 
-# start BBR
-if  [ ! -f "/etc/sysctl.conf.cp" ];then
-  sudo cp /etc/sysctl.conf /etc/sysctl.conf.cp
-  fi
-
-sudo echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
-sudo echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
-sudo sysctl -p
-
 # install and start v2ray
 sudo bash <(curl -L https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh)
 sudo bash <(curl -L https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-dat-release.sh)
